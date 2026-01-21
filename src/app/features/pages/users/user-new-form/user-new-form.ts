c9135283e1f5b2fca9f913380bc0d4767ffd156f
Kevin Fonseca <fonsecakevin74@gmail.com>
2026-01-21 17:03:17 -0500

    [~]usernewform.ts was provided with the formGroup, formControl and HTML was successfully binded

--- src/app/features/pages/users/user-new-form/user-new-form.ts
+++ src/app/features/pages/users/user-new-form/user-new-form.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-new-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-new-form.html',
   styleUrl: './user-new-form.scss',
 })
 export class UserNewForm {
+
+  userNewForm!: FormGroup ;
+
+  constructor(
+  ){
+    this.userNewForm = new FormGroup({
+
+      name: new FormControl("",[Validators.required]),
+      email: new FormControl("",[Validators.required]),
+      password: new FormControl("",[Validators.required]),
+      phone: new FormControl("",[Validators.required]),
+      location: new FormControl("",[Validators.required]),
+      description: new FormControl("",)
+
+      
+    })
+  }
+
+  onSubmit(){
+
+  }
 
 }
