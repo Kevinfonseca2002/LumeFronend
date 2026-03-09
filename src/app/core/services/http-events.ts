9c807d3c27c09a40f3c3eb448be47ea111d73616
Kevin Fonseca <fonsecakevin74@gmail.com>
2026-03-09 17:12:05 -0500

    Implement event fetching in EventCard component; add HttpClient integration and update template for async data binding.

--- src/app/core/services/http-events.ts
+++ src/app/core/services/http-events.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpEvents {
+
+  constructor(
+    private http: HttpClient
+  ){}
+
+  FindAllEvents():Observable<any>{
+    return this.http.get<any>("http://localhost:3000/events").pipe(
+      tap ((data)=>console.log(data)),
+      map ((data=>data.allEvents)),
+      tap ((data)=>console.log(data)
+    )
+  )
+  }
   
 }
--- src/app/shared/layout/event-card/event-card.ts
+++ src/app/shared/layout/event-card/event-card.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { HttpEvents } from '../../../core/services/http-events';
+import { BehaviorSubject, Observable, switchMap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+
 
 @Component({
   selector: 'event-card',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './event-card.html',
   styleUrl: './event-card.scss',
 })
 export class EventCard {
+  events: Observable<any[]> = new Observable<any[]>();
+  events$: BehaviorSubject<void>= new BehaviorSubject<void>(undefined)
+
+
+  constructor(
+    private httpEvents: HttpEvents
+  ){}
+  
+  ngOnInit(){
+    this.events$.pipe(
+      switchMap (()=>this.httpEvents.FindAllEvents())
+    )
+    console.log(this.events, this.events$)
+  }
 
 }
